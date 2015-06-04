384d24fa744e23d6815993f4a986ab17424be403
runefriborg <rune.m.friborg@gmail.com>
2015-06-04 12:32:40 +0200

    Loads data vcf,fasta from m,f and c

--- HLAxPreparePhasing.py
+++ HLAxPreparePhasing.py
@@ -35,26 +35,74 @@ UPDATED="2015-06-03"
 
 def main(args):
 
-    # Read Fasta file
-    handle = gzip.open(args.fafile, "r")    
-    records = list(SeqIO.parse(handle, "fasta"))
-    handle.close()
+    # Read Fasta files
+    faSequence = []
+    for filename in [args.m_fa, args.f_fa, args.c_fa]:
+        if filename[-2:] == 'gz':
+            handle = gzip.open(filename, "r")    
+        else:
+            handle = open(filename, "r")
+        records = list(SeqIO.parse(handle, "fasta"))
+        handle.close()
 
-    if not len(records) == 1:
-        sys.stderr.write("Error!: "+len(records)+" sequences read. Fasta file must have exactly one sequence!\n")
-        sys.exit(1)
+        if not len(records) == 1:
+            sys.stderr.write("Error!: "+str(len(records))+" sequences read. Fasta file '"+ filename +"' must have exactly one sequence!\n")
+            sys.exit(1)
         
-    faSequence = records[0].seq
+        faSequence.append(records[0].seq)
 
 
     # Read vcf file
-    vcf_reader = vcf.Reader(open(args.vcffile, 'r'))
-    vcfRecords = list(vcf_reader)
-    # TODO: organise in an optimal searchable structure
+    vcfRecords = []
+    for filename in [args.m_vcf, args.f_vcf, args.c_vcf]:
+        if filename[-2:] == 'gz':
+            handle = gzip.open(filename, "r")
+        else:
+            handle = open(filename, "r")
+        vcf_reader = vcf.Reader(handle)
+        vcfDict = {}
+        for record in vcf_reader:
+            vcfDict[record.POS] = record
+        handle.close()
 
-    for record in vcfRecords:
-        print(record)
+    vcfRecords.append(vcfDict)
+    
 
+    # Read input file
+    individual = 0                # 0 = male, 1 = female, 2 = child
+    inheritance = ((2),(2),(0,1)) # individuals for which there is inheritance
+
+    for filename in [args.m_input, args.f_input, args.c_input]:
+        if filename[-2:] == 'gz':
+            handle = gzip.open(filename, "r")
+        else:
+            handle = open(filename, "r")
+
+        # Skip header
+        handle.next()
+
+        # Parse lines
+        i = 0
+        for line in handle:
+            try:
+                i += 1
+
+                if line == "\n":
+                    continue
+
+                l = line.strip().split('\t')
+                pos, ref, alt, length, f_pos, m_pos, c_pos = l
+            except ValueError:
+                sys.stderr.write("Error!: Failing parsing line "+str(i)+" in '"+filename+"'!\nGot: "+str(l)+"\n")
+                sys.exit(1)            
+
+            # Get variants
+            #if vcfDict.has_key(int(c_pos)):
+            #    print("ok")
+
+        handle.close()
+
+        individual += 1
 
 
 
@@ -64,19 +112,77 @@ def main(args):
 def usage():
     print("""HLAxPreparePhasing version """+VERSION+""" by Rune M. Friborg (updated """+UPDATED+""")
 Usage:
-  HLAxPreparePhasing --vcf=<file> --fa=<fasta file> --input=<input file>
+  HLAxPreparePhasing --m-vcf=<file> --m-fa=<fasta file> --m-input=<input file> --m-output=<output file> \
+    --f-vcf=<file> --f-fa=<fasta file> --f-input=<input file> --f-output=<output file>\
+    --c-vcf=<file> --c-fa=<fasta file> --c-input=<input file> --c-output=<output file>
 """)
+
 
 class ArgContainer():
     def __init__(self):
-        self.vcffile    = ""
-        self.fafile     = ""
-        self.inputfile  = ""
+        self.m_vcf    = ""
+        self.m_fa     = ""
+        self.m_input  = ""
+        self.m_output  = ""
+        self.f_vcf    = ""
+        self.f_fa     = ""
+        self.f_input  = ""
+        self.f_output  = ""
+        self.c_vcf    = ""
+        self.c_fa     = ""
+        self.c_input  = ""
+        self.c_output  = ""
+
+    def ok(self):
+        err = 0
+        if not self.m_vcf:
+            sys.stderr.write("Missing argument: --m-vcf\n")
+            err = 1
+        if not self.m_fa:
+            sys.stderr.write("Missing argument: --m-fa\n")
+            err = 1
+        if not self.m_input:
+            sys.stderr.write("Missing argument: --m-input\n")
+            err = 1
+        if not self.m_output:
+            sys.stderr.write("Missing argument: --m-output\n")
+            err = 1
+        if not self.f_vcf:
+            sys.stderr.write("Missing argument: --f-vcf\n")
+            err = 1
+        if not self.f_fa:
+            sys.stderr.write("Missing argument: --f-fa\n")
+            err = 1
+        if not self.f_input:
+            sys.stderr.write("Missing argument: --f-input\n")
+            err = 1
+        if not self.f_output:
+            sys.stderr.write("Missing argument: --f-output\n")
+            err = 1
+        if not self.c_vcf:
+            sys.stderr.write("Missing argument: --c-vcf\n")
+            err = 1
+        if not self.c_fa:
+            sys.stderr.write("Missing argument: --c-fa\n")
+            err = 1
+        if not self.c_input:
+            sys.stderr.write("Missing argument: --c-input\n")
+            err = 1
+        if not self.c_output:
+            sys.stderr.write("Missing argument: --c-output\n")
+            err = 1
+
+        if err:
+            sys.stderr.write("\n")
+
+        return not err
+
+        
 
 if __name__ == '__main__':
 
     try:
-        opts, dirs = getopt.getopt(sys.argv[1:], "", ["help", "vcf=", "fa=", "input="])
+        opts, dirs = getopt.getopt(sys.argv[1:], "", ["help", "m-vcf=", "m-fa=", "m-input=", "m-output=", "f-vcf=", "f-fa=", "f-input=", "f-output=", "c-vcf=", "c-fa=", "c-input=", "c-output="])
     except getopt.GetoptError, err:
         # print help information and exit:
         print str(err) # will print something like "option -a not recognized"
@@ -87,19 +193,38 @@ if __name__ == '__main__':
         if a and a[0] == "=":
             a = a[1:]
 
-        if o == "--vcf":
-            args.vcffile = a
-        elif o == "--fa":
-            args.fafile = a
-        elif o == "--input":
-            args.inputfile = a
+     
+        if o == "--m-vcf":
+            args.m_vcf = a
+        elif o == "--m-fa":
+            args.m_fa = a
+        elif o == "--m-input":
+            args.m_input = a
+        elif o == "--m-output":
+            args.m_output = a
+        elif o == "--f-vcf":
+            args.f_vcf = a
+        elif o == "--f-fa":
+            args.f_fa = a
+        elif o == "--f-input":
+            args.f_input = a
+        elif o == "--f-output":
+            args.f_output = a
+        elif o == "--c-vcf":
+            args.c_vcf = a
+        elif o == "--c-fa":
+            args.c_fa = a
+        elif o == "--c-input":
+            args.c_input = a
+        elif o == "--c-output":
+            args.c_output = a
         elif o == "--help":
             usage()
             sys.exit()
         else:
             assert False, "unhandled option"
 
-    if args.vcffile and args.fafile and args.inputfile:
+    if args.ok():
         try:
             main(args)
         except KeyboardInterrupt:
