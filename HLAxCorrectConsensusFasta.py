4c4ff53dec0f934ca473659e8d4834cb0bf8732d
runefriborg <rune.m.friborg@gmail.com>
2015-09-23 14:36:05 +0200

    Completed conflict handling

--- HLAxCorrectConsensusFasta.py
+++ HLAxCorrectConsensusFasta.py
@@ -20,8 +20,8 @@ import os
 ##############################################################
 ####################### Configuration ########################
 
-VERSION="0.02"
-UPDATED="2015-09-22"
+VERSION="0.03"
+UPDATED="2015-09-23"
 PID=str(os.getpid())
 
 ##############################################################
@@ -113,19 +113,26 @@ def main(args):
             refLen = int(entry[1])
             newSeq = entry[2]
 
-            #WARNING! ('5692', '1', 'N') conflicts with previous variant ('5691', '2', 'NN')
-            #WARNING! ('50675', '1', 'AG') conflicts with previous variant ('50674', '2', 'T')
-            #WARNING! ('421484', '1', 'AT') conflicts with previous variant ('421480', '5', 'AGTTG')
 
             if pos < region_start:
                 prev_pos = int(prev_entry[0])
                 prev_refLen = int(prev_entry[1])
                 prev_newSeq = prev_entry[2]
             
+                conflict = False
                 if prev_pos+len(prev_newSeq) > pos:
-                    # Possible conflict with bases. Check.
+                    # Possible conflict with bases. Check.                    
+                    # Example of conflict ('421484', '1', 'AT') conflicts with previous variant ('421480', '5', 'AGTTG')
+                    # Example of non-conflict ('5692', '1', 'N') conflicts with previous variant ('5691', '2', 'NN')
+                    # Example of non-conflict ('50675', '1', 'AG') conflicts with previous variant ('50674', '2', 'T')
+                    j = 0
+                    for i in xrange(pos-prev_pos, len(prev_newSeq)): 
+                        if j < len(newSeq) and prev_newSeq[i] != newSeq[j]:
+                            conflict = True
+                        j += 1
+
+                if conflict:
                     sys.stderr.write("WARNING! " + str(entry) + " conflicts with previous variant " + str(prev_entry) +"\n")                
-                    pass
                 else:
                     # Correct region_start to make room for variant
                     region_start -=  (pos - (prev_pos + prev_refLen))
